017c98b6e80746f650547a3085b1d3495afd71cf
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-07-11 16:51:49 +0200

    feat(ui): simple selector with bubbletea

--- main.go
+++ main.go
@@ -1,7 +1,82 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "charm.land/bubbletea/v2"
+)
+
+type model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func initialModel() model {
+	return model{
+		choices:  []string{"First row", "Second row", "Third row"},
+		cursor:   0,
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "space":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		case "enter":
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	s := "Check out this list\n\n"
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		selected := " "
+		_, ok := m.selected[i]
+		if ok {
+			selected = "x"
+		}
+
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, selected, choice)
+
+	}
+	s += "\nPress q or ctrl+c to exit\n"
+	return tea.NewView(s)
+}
 
 func main() {
-	fmt.Println("Hello world")
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("There is error: %v", err)
+		os.Exit(1)
+	}
 }
